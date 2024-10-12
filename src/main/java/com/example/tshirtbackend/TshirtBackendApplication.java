7cd4884fe92bdbb1c7cb18a4d90ae6c6aa2c95cf
Zakaria <abza0101@student.eafc-uccle.be>
2024-10-12 14:58:30 +0200

    Initial commit

---
+++ src/main/java/com/example/tshirtbackend/TshirtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tshirtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshirtBackendApplication.class, args);
+	}
+
+}
